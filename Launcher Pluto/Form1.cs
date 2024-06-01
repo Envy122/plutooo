092dcf69c766ba563a7ecfe302251ee2b9b40153
Envy122 <helloooguessme@gmail.com>
2024-06-01 21:16:46 +0100

    Add files via upload

---
+++ Launcher Pluto/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher_Pluto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void userControl11_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userControl11.Show();
+            userControl21.Hide();
+            userControl31.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControl11.Show();
+            userControl21.Hide();
+            userControl31.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userControl11.Hide();
+            userControl21.Show();
+            userControl31.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            userControl11.Hide();
+            userControl21.Hide();
+            userControl31.Show();
+        }
+    }
+}
